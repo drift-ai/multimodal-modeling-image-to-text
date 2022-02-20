98443fbf908cec503752c0e61e270f8b13d18fbd
vincent <vclaes1986@gmail.com>
2022-02-20 10:06:55 +0100

    script to debug transformers

---
+++ pipeline-test.py
@@ -0,0 +1,24 @@
+from transformers import pipeline
+from PIL import Image
+import requests
+import torch
+
+classifier = pipeline("image-classification")
+url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+image = Image.open(requests.get(url, stream=True).raw)
+predictions = classifier(url)
+predictions
+
+# from transformers import ViTFeatureExtractor, ViTForImageClassification
+# from PIL import Image
+# import requests
+# import torch
+#
+# url = 'http://images.cocodataset.org/val2017/000000039769.jpg'
+# image = Image.open(requests.get(url, stream=True).raw)
+# feature_extractor = ViTFeatureExtractor.from_pretrained("google/vit-base-patch16-224")
+# inputs = feature_extractor(image, return_tensors="pt")
+# inputs
+# model = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224")
+# outputs = model(**inputs)
+# outputs
